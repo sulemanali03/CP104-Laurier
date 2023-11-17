256237384e8641db345aad767c2c50ed72328c81
sulemanali03 <145292817+sulemanali03@users.noreply.github.com>
2023-11-17 17:10:08 -0500

    Add files via upload

---
+++ Labs/alix4667_l01/t04.py
@@ -0,0 +1,32 @@
+"""
+-------------------------------------------------------
+[program description]
+-------------------------------------------------------
+Author:  Suleman Ali
+ID:      169044667
+Email:   alix4667@mylaurier.ca
+__updated__ = "2023-01-20"
+-------------------------------------------------------
+"""
+# Imports
+
+# Constants
+
+
+def func():
+    """
+    -------------------------------------------------------
+    description
+    Use: 
+    -------------------------------------------------------
+    Parameters:
+        name - description (type)
+    Returns:
+        name - description (type)
+    ------------------------------------------------------
+    """
+
+
+name = input("Please enter your name: ")
+print("Pleased to meet you ")
+print(name)
---
+++ Labs/alix4667_l01/t06.py
@@ -0,0 +1,34 @@
+"""
+-------------------------------------------------------
+[program description]
+-------------------------------------------------------
+Author:  Suleman Ali
+ID:      169044667
+Email:   alix4667@mylaurier.ca
+__updated__ = "2023-01-20"
+-------------------------------------------------------
+"""
+# Imports
+
+# Constants
+
+
+def func():
+    """
+    -------------------------------------------------------
+    description
+    Use: 
+    -------------------------------------------------------
+    Parameters:
+        name - description (type)
+    Returns:
+        name - description (type)
+    ------------------------------------------------------
+    """
+
+
+# Calculate total pay
+salary = 2500.0
+bonus = 1200.0
+pay = salary + bonus
+print('Your pay is', pay)
